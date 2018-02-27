370fc1f3ea372e57096730573218a786737651a1
N0ot-No0t <sustaccosmin544@hotmail.ca>
2018-02-27 10:55:41 -0500

    Add files via upload

---
+++ app/src/main/java/com/patateco/qc/ca/businessmoneyworld/GameActivity.java
@@ -0,0 +1,23 @@
+package com.patateco.qc.ca.businessmoneyworld;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_game);
+
+        //Personnage personnage = new Personnage(22);
+
+    }
+}
