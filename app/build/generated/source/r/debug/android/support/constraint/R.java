13d0972d694831f9ada6aecc671083fbed677be7
N0ot-No0t <sustaccosmin544@hotmail.ca>
2018-02-28 22:33:23 -0500

    revamped UI, added buttons functionalities (kind of)

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -56,7 +56,7 @@ public final class R {
     public static final class id {
         public static final int all = 0x7f07001c;
         public static final int basic = 0x7f07001f;
-        public static final int chains = 0x7f07002a;
+        public static final int chains = 0x7f070025;
         public static final int none = 0x7f07004d;
         public static final int packed = 0x7f070052;
         public static final int parent = 0x7f070053;
--- app/src/main/java/com/patateco/qc/ca/businessmoneyworld/GameActivity.java
+++ app/src/main/java/com/patateco/qc/ca/businessmoneyworld/GameActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -17,7 +21,57 @@ public class GameActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_game);
 
+
+
+        final TextView txtTest = (TextView)findViewById(R.id.txtTest);
+
+        ImageButton btnPers = (ImageButton) findViewById(R.id.imgPersonnage);
+        btnPers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTest.setText("Personnage");
+            }
+        });
+
+        ImageButton btnNews = (ImageButton) findViewById(R.id.imgNews);
+        btnPers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTest.setText("News");
+            }
+        });
+
+        ImageButton btnAction = (ImageButton) findViewById(R.id.imgAction);
+        btnPers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTest.setText("Actions");
+            }
+        });
+
+        ImageButton btnPays = (ImageButton) findViewById(R.id.imgPays);
+        btnPers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTest.setText("Pays");
+            }
+        });
+
+        ImageButton btnNext = (ImageButton) findViewById(R.id.imgNext);
+        btnPers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTest.setText("Next");
+            }
+        });
+
+
+
         //Personnage personnage = new Personnage(22);
+
+
+
+
 
     }
 }
